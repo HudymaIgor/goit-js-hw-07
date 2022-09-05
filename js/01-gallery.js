4a445aabe96fae3381f99d774b700c137f694997
Hudyma_Ihor <Hudyma.work@gmail.com>
2022-09-05 15:50:57 +0300

    first

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,43 @@
-import { galleryItems } from './gallery-items.js';
+    import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galarisEl = document.querySelector( '.gallery' );
+
+const onGalari = ({ preview, original, description } = {}) => {
+    return `<div class="gallery__item">
+  <a class="gallery__link" href="large-image.jpg">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+};
+const galarisСards = galleryItems.map((el) => {
+   return  onGalari(el)
+});
+
+galarisEl.addEventListener('click', (event) => {
+  event.preventDefault();
+
+  if (event.target.nodeName !== "IMG") {
+    return
+  }
+    const instance = basicLightbox.create(`
+    <div class="modal">
+      <img src='${event.target.dataset.source}'/>
+    </div>
+`)
+  window.addEventListener('keydown', event => {
+    if (event.code === 'Escape') {
+        instance.close();
+    }
+    console.log(event);
+  });
+  
+  instance.show()
+})
+
+galarisEl.insertAdjacentHTML("afterbegin", galarisСards.join(''));
