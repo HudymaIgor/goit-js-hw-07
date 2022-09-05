4c5f688371c2d07cd5f3526eb80d36abffc31bc8
Hudyma_Ihor <Hudyma.work@gmail.com>
2022-09-05 17:20:28 +0300

    .find()

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,23 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galarisEl = document.querySelector('.gallery');
+const onGalari = ({ preview, original, description } = {}) => {
 
+    return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`
+
+};
+const galarisСards = galleryItems.map((el) => {
+   return  onGalari(el)
+});
+
+// galarisEl.addEventListener('click', (event) => {
+
+//   instance.show()
+// })
+
+galarisEl.insertAdjacentHTML("afterbegin", galarisСards.join(''));
 console.log(galleryItems);
+
+var lightbox = new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250});
